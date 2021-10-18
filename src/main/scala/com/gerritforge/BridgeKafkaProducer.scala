494d97bf74536112e07bd59d8a50509f311f46f7
Fabio Ponciroli <ponch78@gmail.com>
2021-10-18 19:34:20 +0200

    Bridge to forward messages from Kinesis to Kafka
    Implement message forwarding from Kinesis to Kafka
    
    Bug: Issue 15178
    Change-Id: I5861c99fccee0d63446c3d00c187fc2908a9181a

---
+++ src/main/scala/com/gerritforge/BridgeKafkaProducer.scala
@@ -0,0 +1,27 @@
+package com.gerritforge
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.adapter._
+import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.Producer
+import org.apache.kafka.common.serialization.StringSerializer
+
+case class BridgeKafkaProducer(
+    bridgeConfig: BridgeConfig,
+    actorSystem: ActorSystem[Nothing]
+) {
+
+  private val kafkaBootstrapServers = bridgeConfig.conf
+    .getString("bridge.kafka.bootstrapServers")
+
+  val producerSettings = {
+    ProducerSettings(
+      actorSystem.toClassic,
+      new StringSerializer,
+      new StringSerializer
+    ).withBootstrapServers(kafkaBootstrapServers)
+  }
+
+  val kafkaSink = Producer.plainSink(producerSettings)
+
+}
