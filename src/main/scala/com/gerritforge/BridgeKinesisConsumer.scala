87340021ba2baa2b4c3a2b176a169184b4e59847
Fabio Ponciroli <ponch78@gmail.com>
2021-10-18 19:34:20 +0200

    Bridge to forward messages from Kinesis to Kafka
    Implement message forwarding from Kinesis to Kafka
    
    Bug: Issue 15178
    Change-Id: I5861c99fccee0d63446c3d00c187fc2908a9181a

---
+++ src/main/scala/com/gerritforge/BridgeKinesisConsumer.scala
@@ -0,0 +1,124 @@
+package com.gerritforge
+
+import akka.Done
+import akka.actor.typed.ActorSystem
+import akka.stream.scaladsl
+import com.contxt.kinesis.{ConsumerConfig, KinesisRecord, KinesisSource}
+import com.gerritforge.BrokerBridge.logger
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
+import software.amazon.kinesis.common.{
+  InitialPositionInStream,
+  InitialPositionInStreamExtended
+}
+
+import java.net.URI
+import java.util.UUID
+import scala.concurrent.Future
+
+case class BridgeKinesisConsumer(
+    bridgeConfig: BridgeConfig,
+    actorSystem: ActorSystem[Nothing]
+) {
+
+  val logger: Logger = Logger(
+    LoggerFactory.getLogger(BridgeKinesisConsumer.getClass.getName)
+  )
+  private val region =
+    bridgeConfig.conf.getString("bridge.kinesis.region")
+  private val endpoint =
+    bridgeConfig.conf.getString("bridge.kinesis.endpoint") match {
+      case "" => None
+      case x  => Some(x)
+    }
+  private val appName =
+    bridgeConfig.conf.getString("bridge.kinesis.appName") match {
+      case "" => None
+      case x  => Some(x)
+    }
+
+  lazy val bridgeKinesisConsumerSources
+      : Map[String, scaladsl.Source[KinesisRecord, Future[Done]]] = {
+    bridgeConfig.topics.map { t =>
+      logger.info(s"*** Creating Kinesis Source for topic: $t")
+      t -> {
+        val consumerConfig = ConsumerConfig(
+          streamName = t,
+          appName = s"$appName-$t",
+          workerId = generateWorkerId(),
+          kinesisClient = amazonKinesisAsync,
+          dynamoClient = dynamoDBAsynClient,
+          cloudwatchClient = cloudWatchAsynClient,
+          initialPositionInStreamExtended =
+            InitialPositionInStreamExtended.newInitialPosition(
+              InitialPositionInStream.LATEST
+            ),
+          coordinatorConfig = None,
+          leaseManagementConfig = None,
+          metricsConfig = None,
+          retrievalConfig = None
+        )
+
+        KinesisSource(consumerConfig)
+      }
+    }.toMap
+  }
+
+  private def generateWorkerId() = {
+    s"${
+      import scala.sys.process._
+      "hostname".!!.trim()
+    }:${UUID.randomUUID()}"
+  }
+
+  private val httpClient = NettyNioAsyncHttpClient.builder
+    .maxConcurrency(100)
+    .maxPendingConnectionAcquires(10_000)
+    .build
+
+  private val dynamoDBAsynClient = {
+
+    val clientBuilder = DynamoDbAsyncClient
+      .builder()
+      .httpClient(httpClient)
+      .region(Region.of(region))
+
+    endpoint.map(e => clientBuilder.endpointOverride(new URI(e)))
+    clientBuilder.build
+  }
+
+  private val cloudWatchAsynClient = {
+
+    val clientBuilder = CloudWatchAsyncClient.builder
+      .httpClient(httpClient)
+      .region(Region.of(region))
+
+    endpoint.map(e => clientBuilder.endpointOverride(new URI(e)))
+    clientBuilder.build
+  }
+
+  private val amazonKinesisAsync: KinesisAsyncClient = {
+    val kinesisClientBuilder = KinesisAsyncClient
+      .builder()
+      .httpClient(httpClient)
+      .region(Region.of(region))
+
+    endpoint.map(e => kinesisClientBuilder.endpointOverride(new URI(e)))
+    kinesisClientBuilder.build
+  }
+
+  def handleShutDown() = {
+    sys.ShutdownHookThread {
+      logger.info("\uD83D\uDD0C Shutting down bridge \uD83D\uDD0C")
+      amazonKinesisAsync.close()
+      cloudWatchAsynClient.close()
+      dynamoDBAsynClient.close()
+    }
+  }
+
+}
